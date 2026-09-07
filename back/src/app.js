3ca2aacfa1da68112461060275eb4b784fcce519
robertytocerva <robertytocerva2004@gmail.com>
2026-09-07 17:14:54 -0600

    creacion estructura base

---
+++ back/src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", routes);
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+export default app;
