d4239fde8f16586007eab06d922a5784b281e436
robertytocerva <robertytocerva2004@gmail.com>
2026-09-07 22:14:13 -0600

    update database and backend config

--- back/src/index.js
+++ back/src/index.js
@@ -1,14 +1,29 @@
+import "dotenv/config";
 import app from "./app.js";
 import { config } from "./config/index.js";
+import { runMigrations } from "../db/migrate.js";
 
-const server = app.listen(config.PORT, () => {
-  console.log(`Innova2026 API escuchando en http://localhost:${config.PORT}`);
-});
+const start = async () => {
+  try {
+    console.log("Ejecutando migraciones...");
+    const { applied, total } = await runMigrations({ silent: false });
+    console.log(`Migraciones: ${applied} aplicada(s) de ${total} total.`);
+  } catch (err) {
+    console.error("No se pudieron aplicar las migraciones:", err.message);
+    process.exit(1);
+  }
 
-const shutdown = (signal) => {
-  console.log(`${signal}: cerrando servidor`);
-  server.close(() => process.exit(0));
+  const server = app.listen(config.PORT, () => {
+    console.log(`Innova2026 API escuchando en http://localhost:${config.PORT}`);
+  });
+
+  const shutdown = (signal) => {
+    console.log(`${signal}: cerrando servidor`);
+    server.close(() => process.exit(0));
+  };
+
+  process.on("SIGTERM", () => shutdown("SIGTERM"));
+  process.on("SIGINT", () => shutdown("SIGINT"));
 };
 
-process.on("SIGTERM", () => shutdown("SIGTERM"));
-process.on("SIGINT", () => shutdown("SIGINT"));
+start();
